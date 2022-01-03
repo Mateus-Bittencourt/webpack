9d9a88e9d0148391909ae52b4ad628dbd717aae8
Mateus <mateusmbittencourt@gmail.com>
2022-01-03 16:13:09 -0300

    code togheter

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './styles.scss'
+import img from './matrix.png'
+
+function rootStyle(){
+    const elemRoot = document.getElementById('root')
+    elemRoot.classList.add('container')
+}
+
+function titleComponent() {
+    const elemH1 = document.createElement('h1')
+    elemH1.innerHTML = 'Trololo !'
+    elemH1.classList.add('title')
+    return elemH1
+}
+
+function imageComponent(){
+    const elemImg = new Image(1200,800)
+    elemImg.src = img
+    return elemImg
+}
+
+rootStyle()
+document.body.appendChild(titleComponent())
+document.body.appendChild(imageComponent())
